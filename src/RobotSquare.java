9f3182006e46b482096c2e65907d5e3d9c7a9932
jaLeague <jackie.arseneau@jointheleague.org>
2017-08-13 15:27:53 -0700

    Had to save each recipe file

--- src/RobotSquare.java
+++ src/RobotSquare.java
@@ -1,4 +1,27 @@
+import org.jointheleague.graphical.robot.Robot;
+
+// This recipe draws a square using the Robot
 
 public class RobotSquare {
+    public static void main(String[] args) throws Exception {
+ 
+    	// 1. Make a new Robot
 
+
+        // 3. Put the robot's pen down
+
+
+        // 6. Make the robot move as fast as possible
+
+
+        // 5. Do everything below here 4 times
+
+
+        //         2. Move your robot 200 pixels
+
+
+        //         4. Turn the robot 90 degrees to the right (90 degrees)
+
+
+    }
 }
