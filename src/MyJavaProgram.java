7d7d312ae2260ee1f1845c9198a596e1e9e84726
Buskcoin <keith.groves@jointheleague.org>
2017-11-03 14:03:30 -0700

    Create MyJavaProgram.java

---
+++ src/MyJavaProgram.java
@@ -0,0 +1,13 @@
+import java.awt.Color;
+import org.jointheleague.graphical.robot.Robot;
+
+public class MyJavaProgram {
+	
+	public static void main(String[] args) {
+		
+		// START HERE
+	
+		
+		
+	}
+}
